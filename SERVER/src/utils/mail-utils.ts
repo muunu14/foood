6bab6de97c03beaed3cc8f0176e222d7dcdb76a4
25LP6172 25LP6172 <muunu14@gmail.com>
2026-02-10 08:18:18 +0800

    sss

--- SERVER/src/utils/mail-utils.ts
+++ SERVER/src/utils/mail-utils.ts
@@ -1,20 +1,22 @@
 import nodemailer from "nodemailer";
 import { configDotenv } from "dotenv";
+import { Resend } from "resend";
 configDotenv();
-const { AUTH_EMAIL, AUTH_PASS } = process.env;
-const transport = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: AUTH_EMAIL,
-    pass: AUTH_PASS,
-  },
-});
+// const { AUTH_EMAIL, AUTH_PASS } = process.env;
+// const transport = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: AUTH_EMAIL,
+//     pass: AUTH_PASS,
+//   },
+// });
+const resend = new Resend(process.env.RESEND_API_KEY)
 export const sendVerificationEmail = async (
   receiver: string,
   verifyEmail: string,
 ) => {
-  await transport.sendMail({
-    from: `"Food Delivery App" <${AUTH_EMAIL}>`,
+  await resend.emails.send({
+    from: "onboarding@resend.dev",
     to: receiver,
     subject: "Verify your email",
     html: `
